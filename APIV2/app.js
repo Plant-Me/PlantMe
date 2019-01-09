2070a7e04088d507f7be60f53ad9ef62cd055817
romaric roussel <romaric.roussel@outlook.fr>
2019-01-09 18:38:51 +0100

    feat /plante

--- APIV2/app.js
+++ APIV2/app.js
@@ -77,6 +77,54 @@ app.get("/plante",(req,res) => {
     }  
 })
 
+app.get("/plante",(req,res) => {
+
+  
+  let idTypeParam 
+  let idFamilleParam 
+  const queryString = "select * from plante "
+  const queryType = "select * from type where id = ?"
+  const queryFamille = "select * from famille where id = ?"
+  let idType 
+  let nomType
+  let idFamille 
+  let nomFamille
+  let resultPlant = connection.query(queryString) 
+    if(resultPlant.length > 0){
+      let plante = resultPlant.map((row) => {
+        return {id:row.id, nomFr:row.nomFr, nomLatin:row.nomLatin,
+          couleurFleurs:row.couleurFleurs, exposition:row.exposition,
+          sol:row.sol, usageMilieu:row.usageMilieu,type : {},famille : {}
+        }
+      })
+     for(let i=0;i<resultPlant.length;i++){
+       
+      idTypeParam = resultPlant[i].idType
+      idFamilleParam = resultPlant[i].idFamille
+  
+    //type
+    let resultType = connection. query(queryType,[idTypeParam])
+      idType = resultType[0].id
+      nomType = resultType[0].nom
+    //famille
+    let resultFamille = connection. query(queryFamille,[idFamilleParam])
+      idFamille = resultFamille[0].id
+      nomFamille = resultFamille[0].nom
+
+  //affectation des valeurs
+  
+  plante[i].type = {id:idType,nom:nomType}
+  plante[i].famille = {id:idFamille,nom:nomFamille}
+  //console.log(resultPlant)
+     }
+      
+    res.json(plante)
+    } else {
+      res.sendStatus(204)
+      return
+    }  
+})
+
 app.get("/plante/:id",(req,res) => {
   
  
