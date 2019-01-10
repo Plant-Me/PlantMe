8ce1abd25c6119e1690c76adb539e0832a72daed
romaric roussel <romaric.roussel@outlook.fr>
2019-01-10 17:44:34 +0100

    feat:sauvegarde

--- APIV2/app.js
+++ APIV2/app.js
@@ -112,24 +112,42 @@ app.get("/utilisateur/:idUtilisateur/plante",(req,res) => {
 })
 
 app.get("/utilisateur/:idUtilisateur/plante/:idPlanteUtilisateur",(req,res) => {
+  const planteId = req.params.id
+  const queryString = "SELECT idPlante,nomFr,nomLatin,description,couleurFleurs,exposition,sol,usageMilieu,famille.idFamille,famille.nom as nomFamille,image.idImage,image.url,type.idType,type.nom from plante inner JOIN famille on plante.id_famille = famille.idFamille INNER JOIN image on plante.id_image = image.idImage INNER join type on plante.id_type = type.idType where plante.idPlante = ?"
+  const queryString2 = "SELECT * FROM plante_calendrier inner join action_calendrier on action_calendrier.idActionCalendrier = plante_calendrier.id_action_calendrier inner join mois on mois.idMois = plante_calendrier.id_mois WHERE plante_calendrier.id_plante = ?"
+  
+  let resultquery1 = connectionSync.query(queryString,[planteId])
+    if (resultquery1.length > 0){
+      let plante = resultquery1.map((row) => {
+        return {idPlante:row.idPlante, nomFr:row.nomFr, nomLatin:row.nomLatin,description:row.description,
+          couleurFleurs:row.couleurFleurs, exposition:row.exposition,
+          sol:row.sol, usageMilieu:row.usageMilieu,type : {idType:row.idType,nom:row.nom},image : {idImage:row.idImage,url:row.url},
+          famille : {idFamille:row.idFamille,nom:row.nomFamille}, 
+          actions : []
+      
+        }
+      })
+        let resultquery2 = connectionSync.query(queryString2,[planteId])
+        if (resultquery1.lenght > 0){
+          res.sendStatus(404)
+          return
+        }else {
+          for(let i=0;i<resultquery2.length;i++){
 
-  const utilisateurId = req.params.idUtilisateur
-  const idPlanteUtilisateur = req.params.idPlanteUtilisateur
-
-  const queryString = "select plante.idPlante,plante.id_image,image.url,plante.id_type,type.nom,plante.usageMilieu,plante.nomFr,plantes_utilisateur.id_plante_utilisateur,plantes_utilisateur.nom_personnel "
-  + "from plante inner join image on plante.id_image = image.idImage INNER join plantes_utilisateur on plante.idPlante = plantes_utilisateur.id_plante INNER join type on plante.id_type = type.idType where plantes_utilisateur.id_utilisateur = ? "
-  connectionAsync.query(queryString,[utilisateurId],(err,rows,fields)=> {
-    if (err){
-      console.log("error " + err)
+            plante[0].actions[i] = {
+              idActionCalendrier : resultquery2[i].idActionCalendrier,
+              idType : resultquery2[i].type,
+              idMois : resultquery2[i].idMois,
+              mois : resultquery2[i].nom      
+            }      
+          }
+        }
+        res.json(plante)
+      }else {
       res.sendStatus(404)
       return
-    }
-    const plante = rows.map((row) => {
-      return {idPlante:row.idPlante,idPlanteUtilisateur:row.id_plante_utilisateur, nomFr:row.nomFr,nomPersonnel:row.nom_personnel, usageMilieu:row.usageMilieu,
-        image :{idImage:row.id_image,url:row.url},type:{idType:row.id_type,nom:row.nom}}
-    })
-    res.json(plante)
-  })   
+  }
+    
 })
 //localhost:3003
 app.listen(3000, () => {
