7f92a0c46d061a5c89244bcf901e2b05a36f3455
romaric roussel <romaric.roussel@outlook.fr>
2019-01-21 11:46:25 +0100

    fix

--- app.js
+++ app.js
@@ -12,12 +12,14 @@ const bodyParser= require('body-parser')
   host: 'db4free.net',
   user: 'roussel',
   password:"azeqsdwxc",
+  port:'3306',
   database: 'plantemes'
 })
 const connectionSync = new mysqlSync({
   host: 'db4free.net',
   user: 'roussel',
   password:"azeqsdwxc",
+  port:'3306',
   database: 'plantemes'
 });
 
